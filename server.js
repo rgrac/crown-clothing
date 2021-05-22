8bed36f0dd830dcad64c8b5d0cb7877337452fd1
rgrac <gracia.rafael@hotmail.com>
2021-05-22 23:57:23 +0300

    added compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const compression = require("compression");
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build','index.html'))
-    });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-app.listen(port, error => {
-    if (error) throw error;
-    console.log('Server running om port' + port);
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log("Server running om port" + port);
 });
 
-app.post('/payment', (req, res) => {
-    const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: 'usd',
-    };
-    stripe.charges.create(body, (stripeErr, stripeRes) =>{
-        if (stripeErr) {
-            res.status(500).send({ error: stripeErr })
-        } else {
-            res.status(200).send({ success: stripeRes })
-        }
-    })
-})
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
